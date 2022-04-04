a0d38a87f8dc9287d7c21d5359b8a75721af86a1
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-04-04 15:44:27 +0600

    router add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import useReviewes from './Hooks/useReviews';
+import Blogs from './Components/Blogs/Blogs';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Reviews from './Components/Reviews/Reviews';
+import usereviews from './Hooks/useReviews';
 
 function App() {
-  const [reviewes, setReviewes] = useReviewes();
-  console.log(reviewes)
+  const [reviews, setreviews] = usereviews();
+  console.log(reviews)
   return (
-    <p>reviewes</p>
+    <div>
+      <h1>Lost In Mountain</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element = {<Home></Home>}></Route>
+        <Route path='/reviews' element = {<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element = {<DashBoard></DashBoard>}></Route>
+        <Route path='/blogs' element = {<Blogs></Blogs>}></Route>
+      </Routes>
+    </div>
+    
   );
 }
 
