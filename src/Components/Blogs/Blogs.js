3f631700c104e6f8641de4de4bfbddd3c3d5624d
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-04-05 06:41:06 +0600

    add two chart and one qusans

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            
+            <h1 className='text-7xl text-center font-semibold text-green-900 my-8'>Blogs</h1>
+            <div>
+                <h1>Block, Inline and Inline-block</h1>
+                <p>
+                    Block element is which take the full width of it's parent. And It always starts in new line. We can set height, width of block element. 
+                    <br></br>
+                    Inline element takes width as much as it's content needs. We can't set height or width of inline element. It doesn't start in new line.
+                    <br></br>
+                    Inline-block element is the combination of both inline and block element. It'll take width as it's content needs. But we can set the height and width of inline-block element.
+
+                </p>
+            </div>
         </div>
     );
 };
--- src/Components/DashBoard/DashBoard.js
+++ src/Components/DashBoard/DashBoard.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip , Area, AreaChart, Legend} from 'recharts';
 
 const DashBoard = () => {
+    const [profile, setProfile] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProfile(data))
+    }, [])
     return (
         <div>
-            
+            <h1 className='text-7xl text-center font-semibold text-green-900 my-8'>DashBoard</h1>
+            <div className='grid grid-cols-1-sm grid-cols-2 gap-4'>
+
+                <div>
+                    <h1 className='text-4xl text-red-700 font-medium'>Month Vs Sell</h1>
+                    <LineChart width={500} height={300} data={profile} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                        <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                    </LineChart>
+                </div>
+                <div>
+                    <h1 className='text-4xl text-red-700 font-medium'>Investment and Revenue Vs month</h1>
+                    <AreaChart width={500} height={300} data={profile}
+                        margin={{ top: 10, right: 20, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <Legend verticalAlign="top" height={36}/>
+                        <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                        <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                    </AreaChart>
+                </div>
+            </div>
+
+
         </div>
     );
 };
